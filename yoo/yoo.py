01657a617d57c4a9a4d7436016e5b6ba433636a7
Mike Busuttil <mikebusuttil@gmail.com>
2023-09-30 03:19:30 -0400

    get messgaes into body control

--- yoo/yoo.py
+++ yoo/yoo.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-import time
+import time, json
+from cereal import messaging
 
 def main():
-  while True:
-    print("yoooooooooooooooooo")
-    time.sleep(1)
+  pm = messaging.PubMaster(['customReservedRawData1'])
+  for i in range(2):
+    time.sleep(2)
+    msg = messaging.new_message()
+    msg.customReservedRawData1 = json.dumps({"hi": "bro", "whats": "up"}).encode()
+    pm.send('customReservedRawData1', msg)
+  
 
 if __name__ == "__main__":
   main()

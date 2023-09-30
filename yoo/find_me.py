0611c4e047f58968d879a7a6dbf780083ebe2fc0
Mike Busuttil <mikebusuttil@gmail.com>
2023-09-30 17:51:18 -0400

    face following algo (untested)

---
+++ yoo/find_me.py
@@ -0,0 +1,69 @@
+import cv2, face_recognition
+
+me = cv2.imread("./yoo/1tile.png")
+# me = cv2.imread("./yoo/2.5tiles.png")
+me = cv2.cvtColor(me, cv2.COLOR_BGR2RGB)
+top, right, bottom, left = face_recognition.face_locations(me)[0]
+# cv2.imshow('face', me[top:bottom, left:right])
+# cv2.waitKey(0)
+me = face_recognition.face_encodings(me)[0]
+
+face_height_pixels = bottom - top
+face_width_pixels = right - left
+print("H x W", face_height_pixels, face_width_pixels)
+# face_width_inches = 5.5
+# face_height_inches = 8.5
+# tile length = 36"
+
+height = 1208
+half_width = 1928/2
+max_size = 120
+min_size = 40
+size_range = max_size - min_size
+
+def locate_match(frame):
+    locations = face_recognition.face_locations(frame)
+    for top, right, bottom, left in locations:
+        face = frame[top:bottom, left:right]
+        face = face_recognition.face_encodings(face)
+        if face_recognition.compare_faces([me], face):
+            return top, right, bottom, left
+        
+def calculate_distance(top, right, bottom, left):
+    '''
+    TODO:
+      - take lots of measurements to determine distance
+      - use intrinsic matrix to un-distort the image first
+    '''
+    return None
+
+def get_yaw(image_center):
+    offset = half_width - image_center
+    power = offset / half_width
+    return min(power, 1)
+
+def get_acceleration(size):
+    if size > max_size:
+        return 0
+    if size < min_size:
+        return 1
+    return (size - min_size) / size_range
+
+def main_loop(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+
+    location = locate_match(frame)
+    if not location:
+        #TODO don't just full stop if you don't see me
+        # might also be good for CPU utilization to go with the current trajectory for a couple frames without processing
+        # good UX might be to average over the last 5 (or so) frames
+        return
+    
+    _, right, _, left = location
+    center = (right + left)/2
+    size = right - left
+
+    yaw_power = get_yaw(center)
+    forward_power = get_acceleration(size)
+
+    move_to(forward_power, yaw_power)
